2177545a63def54e4e60a06b7404a44c6b7e0e92
tsheeezy <mcshenzinator@gmail.com>
2015-04-23 15:40:06 -0700

    Fixed showUserControl thingy for discrete bar charts

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -48,9 +48,9 @@ $.ajax({
 
                $('#graph-bar').on('click',function(){
                   clearDrawing();
-                  drawMultiBarChart(multiData, '#line_chart0', dt);
-                  drawMultiBarChart(formatted_data1,'#line_chart', dt);
-                  drawMultiBarChart(formatted_data2, '#line_chart2', dt);
+                  drawMultiBarChart(multiData, '#line_chart0', dt, true);
+                  drawMultiBarChart(formatted_data1,'#line_chart', dt, false);
+                  drawMultiBarChart(formatted_data2, '#line_chart2', dt, false);
                });
 
                $('#graph-scatter').on('click',function(){
@@ -71,15 +71,21 @@ $.ajax({
        });
 }
 
-function drawMultiBarChart(data, graph_name, dt) {
+function drawMultiBarChart(data, graph_name, dt, multi) {
     var chart = nv.models.multiBarChart()
       .margin({left: 100})
       .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
       .rotateLabels(0)      //Angle to rotate x-axis labels.
-      .showControls(true)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
+      //.showControls(true)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
       .groupSpacing(0.1)    //Distance between each group of bars.
     ;
 
+    if(multi){ // Multi
+      chart.showControls(true);
+    }
+    else{ // Single
+      chart.showControls(false);
+    }
     chart.xAxis
         .tickFormat(d3.format('f'));
 
