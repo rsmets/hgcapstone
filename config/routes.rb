f6178a687c3b6b2ada54e3b59cfd41bdcbd8b91f
Sean Spearman <seanmspearman@gmail.com>
2015-05-27 20:09:58 -0700

    Clean up explore select tag and remove unused scaffolds.
    - Switch h2 to a label for the select tag and change its id to be more consistent
    - Remove 'Gongoozler' option to avoid promoting gongoozling.
    - Change 'Kendall' to show that it doesn't work yet. Change it back when it works.
    - Remove all that stuff from last quarter (e.g. TimeSlices) that we don't use. Hope this doesn't break anything. Doesn't seem to have done so.

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,6 @@ Rails.application.routes.draw do
 
   get 'data_menu/index'
 
-  resources :time_slices
-
-  resources :states
-
-  resources :posts
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
